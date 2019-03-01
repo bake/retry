7c98e8d7eeeb51eaf1584cbcaae53559dcb2ebe0
bake <bake@192k.pw>
2019-03-01 18:04:22 +0100

    example wording

--- retry_test.go
+++ retry_test.go
@@ -13,8 +13,9 @@ import (
 
 // Create a new http.Client that will retry requests five times and sleeps
 // one second between each one.
-// Since the requested URL will always fail, this function will run for five
-// seconds and the time it takes to call the endpoint five times.
+// Since the requested URL will always return a status code outside of
+// [200, 300[, this function will run for five seconds and the time it takes to
+// call the endpoint five times.
 func ExampleNew() {
 	client := &http.Client{
 		Transport: retry.New(5, time.Second, nil),
